58000a209158e1556b8d952f38434a0337d57622
Jordan Ball <jordan@cjordanball.info>
2018-07-09 10:19:55 -0400

    create basic gcw site

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import styles from './styles/main.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles/main.css';
+import GCW from './components/gcw';
 
-console.log('testing');
+const gcw = GCW();
+
+ReactDOM.render(
+    gcw, document.querySelector('#root')
+);
