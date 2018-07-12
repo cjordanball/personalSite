dc7956414650db99f88517950af6e9fe51ecf094
Jordan Ball <jordan@cjordanball.info>
2018-07-12 08:34:55 -0400

    add favicon to site

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './styles/styles.less';
 import GCW from './components/gcw';
+import './assets/images/ballIcon.ico';
 
 const gcw = GCW();
 
