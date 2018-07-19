0fe884fc7d7e4576a6217baa737fbb3d21ee6362
Jordan Ball <jordan@cjordanball.info>
2018-07-19 14:05:48 -0400

    add esLint

--- src/components/link.js
+++ src/components/link.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const HeadLink = (props) => {
-    console.log('hlprops: ', props
-);
-    return (
-        <li className="headLink">
-            <a href={props.props.href}>{props.props.name}</a>
-        </li>
-    );
-}
+const HeadLink = ((props) => {
+	const { href } = props.props;
+	const { name } = props.props;
+	return (
+		<li className="headLink">
+			<a href={href}>{name}</a>
+		</li>
+	)
+});
 
 export default HeadLink;
--- src/components/subhead.js
+++ src/components/subhead.js
@@ -2,8 +2,8 @@ import React from 'react';
 import HeadLink from './link';
 
 const SubHeader = ((props) => {
-    const targets = props.targets;
-    console.log('targ: ', targets);
+	const { targets } = props;
+	console.log('targ: ', targets);
 
     const linkItems = targets.map(target => {
         return <HeadLink props={target} key={target.name}/>
