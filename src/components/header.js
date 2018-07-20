634ed199daf3029ecb807281d2f47f632d18886e
Jordan Ball <jordan@cjordanball.info>
2018-07-19 23:26:05 -0400

    add eslint

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Header = () => (
-    <div className="header">
-        <h3 className="leader">Adventures in Javascript</h3>
-        <h1 className="headline">C. Jordan Ball III</h1>
-    </div>
+	<div className="header">
+		<h3 className="leader">
+			{'Adventures in Javascript'}
+		</h3>
+		<h1 className="headline">
+			{'C. Jordan Ball III'}
+		</h1>
+	</div>
 );
 
 export default Header;
